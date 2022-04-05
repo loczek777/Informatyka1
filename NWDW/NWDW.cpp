95b4523696e4c524ac75f29a403c92ecfebab664
loczek777 <leagueoffpoland@interia.pl>
2022-04-05 10:01:47 +0200

    NWDW

---
+++ NWDW/NWDW.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a, b, nww, il;
+
+	cout << "Podaj dwie liczby: ";
+	cin >> a >> b;
+
+
+	il = a * b;
+
+	while (a != b)
+		if (a < b)
+			b = b - a;
+		else
+			a = a - b;
+
+
+
+
+	cout << "NWD = " << a << endl;
+	nww = il / a;
+	cout << "NWW = " << nww << endl;
+	return 0;
+}
