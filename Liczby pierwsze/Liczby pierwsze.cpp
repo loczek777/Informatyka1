b048507ff4b3226874d1595924ee73cc0c04ba9b
loczek777 <leagueoffpoland@interia.pl>
2022-04-05 09:34:56 +0200

    Liczby Pierwsze

---
+++ Liczby pierwsze/Liczby pierwsze.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    int lp = 0;
+    int p = 2;
+    int h;
+    int d;
+    bool x;
+    cout << "Podaj ilosc liczb: " << endl;
+    cin >> h;
+
+    while (lp < h) {
+        x = true;
+        for (int d = 2; d <= p - 1; d++) {
+            if (p % d == 0) {
+                x = false;
+            }
+        }
+        if (x == true) {
+            cout << p << " :Jest to liczba pierwsza" << endl;
+            lp = lp + 1;
+        }
+        p = p + 1;
+    }
+}
