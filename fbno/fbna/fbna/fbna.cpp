14ce8f62d5c4bf822306d9d5de0e061437dd255f
loczek777 <leagueoffpoland@interia.pl>
2022-05-10 10:36:34 +0200

    fbno

---
+++ fbno/fbna/fbna/fbna.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+void Funkcja(int n);
+
+int main()
+{
+	int n;
+	cout << "Ile ciagów wypisać: ";
+	cin >> n;
+	Funkcja(n);
+}
+
+void Funkcja(int n) {
+	int f;
+	int f0 = 0;
+	int f1 = 1;
+
+	for (int i = 0; i < n; i++) {
+		if (i > 1) {
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+			f = i;
+		}
+		cout << f << endl;
+	}
+}
